52911a2585f09a552c6cecdc0b2b56b0c18440e8
Carlos Ivis <carlosivis1@gmail.com>
2026-01-27 23:49:47 -0300

    refactor: Use collectAsStateWithLifecycle for state collection

--- composeApp/src/commonMain/kotlin/dev/carlosivis/workoutsmart/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/carlosivis/workoutsmart/screens/home/HomeScreen.kt
@@ -29,13 +29,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.carlosivis.workoutsmart.Utils.Dimens
 import dev.carlosivis.workoutsmart.Utils.FontSizes
 import dev.carlosivis.workoutsmart.Utils.Shapes
@@ -61,7 +61,7 @@ import org.jetbrains.compose.resources.stringResource
 fun HomeScreen(
     viewModel: HomeViewModel,
 ) {
-    val state by viewModel.state.collectAsState()
+    val state by viewModel.state.collectAsStateWithLifecycle()
     val action: (HomeViewAction) -> Unit = viewModel::dispatchAction
     Content(
         state = state,
